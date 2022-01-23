3c776b1c155014db9514b4a23e6420198c51cee1
zlyk <test@mail.ru>
2022-01-23 14:44:16 +0300

    reg form

--- main.py
+++ main.py
@@ -1,18 +1,50 @@
 import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.uic import loadUi
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-
-class MyWindow(QMainWindow):
+class Login(QDialog):
     def __init__(self):
-        super().__init__()
-        uic.loadUi('LogIn.ui', self)
+        super(Login,self).__init__()
+        loadUi("login.ui",self)
+        self.loginbutton.clicked.connect(self.loginfunction)
+        self.password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.createaccbutton.clicked.connect(self.gotocreate)
+
+    def loginfunction(self):
+        email=self.email.text()
+        password=self.password.text()
+        print("Successfully logged in with email: ", email, "and password:", password)
+
+    def gotocreate(self):
+        createacc=CreateAcc()
+        widget.addWidget(createacc)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class CreateAcc(QDialog):
+    def __init__(self):
+        super(CreateAcc,self).__init__()
+        loadUi("createacc.ui",self)
+        self.signupbutton.clicked.connect(self.createaccfunction)
+        self.password.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.confirmpass.setEchoMode(QtWidgets.QLineEdit.Password)
+
+    def createaccfunction(self):
+        email = self.email.text()
+        if self.password.text()==self.confirmpass.text():
+            password=self.password.text()
+            print("Successfully created acc with email: ", email, "and password: ", password)
+            login=Login()
+            widget.addWidget(login)
+            widget.setCurrentIndex(widget.currentIndex()+1)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyWindow()
-    ex.show()
-    sys.exit(app.exec_())
 
+app=QApplication(sys.argv)
+mainwindow=Login()
+widget=QtWidgets.QStackedWidget()
+widget.addWidget(mainwindow)
+widget.setFixedWidth(480)
+widget.setFixedHeight(620)
+widget.show()
+app.exec_()
